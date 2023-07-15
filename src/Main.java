f91ccb57b746e9075314e7a71441910d42d04bdc
Rilke <deathrilke@gmail.com>
2023-07-15 16:15:07 -0400

    Mise a jour du main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TeslaUserService userService = new TeslaUserServiceMock();
+
+        double percentage = 0.25; // 25%
+        /**Liste de personne éligible*/
+        List<TeslaUser> usersEligible = userService.getAdminissibleUsers();
+
+        /**Liste  de 25% de personnes éligiblent */
+        List<TeslaUser> selectedUsers = TeslaUserFilter.filterUsers(usersEligible, percentage);
+
+        /**
+         * Affichage de la liste
+         */
+        System.out.println("Utilisateurs de tesla éligible:");
+        for (TeslaUser user : selectedUsers) {
+            System.out.println("Email:"+ user.getIdentifiant()+ " Score: "+user.getScore());
+        }
     }
 }
